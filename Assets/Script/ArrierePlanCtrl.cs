e37d511f2d8220da7fb32b4075c74866f97d35b4
Guillaume <guillaume@chief>
2020-03-11 15:05:33 -0400

    debut de l'expirimentation

--- Assets/Script/ArrierePlanCtrl.cs
+++ Assets/Script/ArrierePlanCtrl.cs
@@ -6,10 +6,26 @@ public class ArrierePlanCtrl : MonoBehaviour
 {
     [SerializeField]
     GameObject[] ArrierePlan;
+    [SerializeField]
+    GameObject personnagePrincipale;
     private Camera cameraJoueur;
     private Vector2 tailleEcran;
+    private int childNeded;
+    private List<ArrierePlan> arrierePlans = new List<ArrierePlan>();
+    //ArrierePlan
+    public class ArrierePlan{
+        private int cadre{get; set;}
+        private GameObject image{get; set;}
+
+        public ArrierePlan(int cadre, GameObject image){
+            this.cadre = cadre;
+            this.image = image;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
+
     {
         cameraJoueur = gameObject.GetComponent<Camera>();
         tailleEcran = cameraJoueur.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cameraJoueur.transform.position.z));
@@ -20,19 +36,30 @@ public class ArrierePlanCtrl : MonoBehaviour
 
     void chargerArrierePlan(GameObject spriteArrierePlan){
         float largeurSprite = spriteArrierePlan.GetComponent<SpriteRenderer>().bounds.size.x;
-        int childNeded = (int)Mathf.Ceil(tailleEcran.x * 2 / largeurSprite);
+        childNeded = (int)Mathf.Ceil(tailleEcran.x * 2 / largeurSprite);
         GameObject clone = Instantiate(spriteArrierePlan) as GameObject;
-        for(int i = 0; i <= childNeded; i++){
-            GameObject c = Instantiate(clone) as GameObject;
-            c.transform.position = new Vector3(largeurSprite * i, spriteArrierePlan.transform.position.y, spriteArrierePlan.transform.position.z);
-            c.name = spriteArrierePlan.name + i;
-        }
+        genèreArrièrePlan(clone);
         Destroy(clone);
         Destroy(spriteArrierePlan.GetComponent<SpriteRenderer>());
     }
     // Update is called once per frame
     void Update()
     {
-        
+        gererArrièrePlan();
+    }
+
+    void genèreArrièrePlan(GameObject clone){
+        for(int i = 0; i <= childNeded; i++){
+            GameObject c = Instantiate(clone) as GameObject;
+            c.transform.position = new Vector3(largeurSprite * i, spriteArrierePlan.transform.position.y, spriteArrierePlan.transform.position.z);
+            c.name = spriteArrierePlan.name + i;
+            arrierePlans.add(new ArrierePlan(i, c))
+        }
+    }
+
+    void gèreArrièrePlan(){
+        foreach(ArrierePlan spriteArrierePlan in arrierePlans){
+            
+        }
     }
 }
--- Assets/Script/balleCtrl.cs
+++ Assets/Script/balleCtrl.cs
@@ -10,7 +10,7 @@ public class balleCtrl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 3);
+        Destroy(this.gameObject, 1.5f);
     }
 
     // Update is called once per frame
